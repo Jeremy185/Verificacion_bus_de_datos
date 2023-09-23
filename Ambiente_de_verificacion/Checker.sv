1ba61288d3a4c3f7f7b4fe61c4601f2f72b2ae6d
Jeremy185 <110881234+Jeremy185@users.noreply.github.com>
2023-09-23 17:24:15 -0600

    Creacion algunos modulos que son parte del diagrama del ambiente

---
+++ Ambiente_de_verificacion/Checker.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/23/2023 05:19:39 PM
+// Design Name: 
+// Module Name: Checker
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Checker(
+
+    );
+endmodule
