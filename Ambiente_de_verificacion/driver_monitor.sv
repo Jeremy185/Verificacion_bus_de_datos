f9ccf0c55b263320ba2c935d1b7968d691429af9
Jeremy185 <110881234+Jeremy185@users.noreply.github.com>
2023-09-29 15:03:22 -0600

    Clases monitor, driver y driver_monitor

--- Ambiente_de_verificacion/driver_monitor.sv
+++ Ambiente_de_verificacion/driver_monitor.sv
@@ -5,8 +5,9 @@ class monitor #(parameter width = 16, parameter depth = 8, parameter drivers = 4
     trans_bus_mbx agente_monitor;
     virtual FIFO #(.width(width)) fifo_out;
 
-    trans_bus cola_out[$]; //Declaracion del queue del monitor
-    int id;
+    bit [width-1:0] cola_out[$]; //Declaracion del queue del monitor que solo almacenas paquetes recibidos
+    int   id;   //El id propio de cada terminal en logic para poderlo comparar con los ID de los paquetes
+
 
     function new(int terminal);
         this.id = terminal;
@@ -16,8 +17,35 @@ class monitor #(parameter width = 16, parameter depth = 8, parameter drivers = 4
     task run();
         $display("[%g] La FIFO de salida %d fue inicializada",$time, id );
         
+        forever begin
+            trans_bus #(.width(width), .max_drivers(drivers)) transaccion;
+            $display("[%g] La FIFO de salida %d espera por una transacción",$time, id);
+            espera = 0;  //No creo que el retardo sea relevante en este caso
+            
+            @(posedge fifo_out.clk);
+            agente_monitor.get(transaccion);
+            transaccion.print_out("FIFO: Transaccion recibida");
+            $display("Transacciones pendientes en el mailbox agente_monitor %d = %g",id,agente_monitor.num());
+
+            while(fifo_out.push == 0)begin
+                @(posedge fifo_out.clk);
+            end
+
+            case(transaccion.tipo)
+                envio: begin
+                    cola_out.push_back(fifo_out.D_push); //Aqui cuando detecta una señal de pull entonces envia el dato.
+                    $display("FIFO out %d recibio el dato ")
+                    //transaccion.print_out("Driver: Transaccion ejecutada");
+                end
+            endcase
+
+
+        end
     endtask
 endclass
+
+
+
 
 class driver #(parameter width = 16, parameter depth = 8, parameter drivers = 4);
     trans_bus_mbx agente_driver;
@@ -33,15 +61,17 @@ class driver #(parameter width = 16, parameter depth = 8, parameter drivers = 4)
     endfunction 
 
     task run();
-        $display("[%g] El driver %d fue inicializado",$time, id );
+        $display("[%g] La FIFO de entrada %d fue inicializada",$time, id );
         @(posedge fifo_in.clk);
         fifo_in.rst = 1;
         @(posedge fifo_in.clk);
 
         forever begin 
-            trans_fifo #(.width(width), .max_drivers(drivers)) transaccion;
-            fifo_in.pnding = 0;   //Pongo la señal de pending que va a la entrada del dut en 0
+            trans_bus #(.width(width), .max_drivers(drivers)) transaccion;
+            fifo_in.pnding = 0;  //Pongo la señal de pending que va a la entrada del dut en 0
             fifo_in.D_pop  ='0;  //Pongo todos los bits del paquete en 0 bits 
+            fifo_in.rst    = 0;  //Pongo el reset en 0.
+
 
             $display("[%g]El driver %d espera por transaccion", $time, id);
             espera = 0;
@@ -49,7 +79,7 @@ class driver #(parameter width = 16, parameter depth = 8, parameter drivers = 4)
             @(posedge fifo_in.clk);
             agente_driver.get(transaccion); //obtengo la transaccion del mailbox
             transaccion.print("Driver: Transaccion recibida");
-            $display("Transacciones pendientes en el mailbox agente_driver %d = %g",agente_driver.num());
+            $display("Transacciones pendientes en el mailbox agente_driver %d = %g",id,agente_driver.num());
             
             while(espera < transaccion.retardo)begin //Hago un retardo
                 @(posedge fifo_in.clk);
@@ -72,6 +102,31 @@ class driver #(parameter width = 16, parameter depth = 8, parameter drivers = 4)
 
 endclass
 
-class driver_monitor #(parameter width = 16, parameter depth = 8);
+class driver_monitor #(parameter width = 16, parameter depth = 8, parameter drivers = 4);
+    trans_bus_mbx agente_driver;
+    trans_bus_mbx agente_monitor;
+    monitor #(.width(width), .depth(depth), .drivers(drivers)) inst_monitor;
+    driver #(.width(width), .depth(depth), .drivers(drivers)) inst_driver;
+    int id;
 
+    function new(int terminal);
+        this.id = terminal;
+        //instanciacion del driver y el monitor
+        inst_driver = new(terminal);
+        inst_monitor = new(terminal);
+
+        //Conexion de los mailboxes
+        inst_monitor.agente_monitor = agente_monitor;
+        inst_driver.agente_driver = agente_driver;
+    endfunction 
+
+
+    virtual task run();  //Aqui corro en paralelo el driver y el monitor (en el ambiente se corre en paralelo solo los drivers_monitores)
+        $display("[%g] El driver_monitor fue inicializado [%d]", $time, id)
+        fork
+            inst_monitor.run();
+            inst_driver.run();
+        join_none
+    endtask
+    
 endclass
